783e182839a207da242647ddbbc6323ae8ccf55a
JDCG00 <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-10-28 08:42:04 +0200

    v1

--- php/operaciones.php
+++ php/operaciones.php
@@ -1,10 +1,10 @@
 <?php
     switch ($_GET['op']) {
         case 'b':
-            header("Location:baja2.php");
+            header("Location:baja2.php?id=".$_GET['id']."");
             break;
         case 'm':
-            header("Location:modificar2.php");
+            header("Location:modificar2.php?id=".$_GET['id']."");
             break;
         case 'a':
             header("Location:alta.php");
